166a2cc26f7017aceda19f0811d81d76e958f807
Anthony Kesich <akesich+github@gmail.com>
2019-08-20 03:10:41 -0500

    made FYB sign able to run multiple effects at a randomized interval

--- 2019 Burn/FYB/FYB.ino
+++ 2019 Burn/FYB/FYB.ino
@@ -2,30 +2,26 @@
 
 FASTLED_USING_NAMESPACE
 
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
-
-#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
-#warning "Requires FastLED 3.1 or later; check github for latest code."
-#endif
-
 const int flicker_intervals[] = {10, 20, 20, 240, 20, 40, 20, 100, 20, 20, 20, 260, 80, 20, 240, 60, 160, 20, 240, 20, 1000, 20, 20, 40, 100, 20}; //, 2740, 340, 860, 20, 1400, 20, 60, 20};
 
 #define DATA_PIN    6
-//#define CLK_PIN   4
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS    150
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS          64
+#define BRIGHTNESS          64 // out of 255
 #define FRAMES_PER_SECOND  120
+
+#define MIN_EVENT_SPACING 45  // seconds
+#define MAX_EVENT_SPACING 150 // seconds
+
+#define FUCK_YOU_DURATION 18 // seconds
+#define HEART_YOU_DURATION 12 // seconds
+#define FUCK_OUR_BURN_DURATION 15 // seconds
+
+#define MAX_SPARKLE_CHANCE 90
+#define MAX_SPARKLE_DISTANCE 5
 
 typedef struct {
   uint8_t x;
@@ -34,7 +30,7 @@ typedef struct {
   uint8_t let_num;
 } SignLED;
 
-SignLED ledInfo[NUM_LEDS] = {
+const SignLED ledInfo[NUM_LEDS] = {
   // Fuck
   {10, 43, 0, 0},
   {10, 35, 0, 0},
@@ -215,162 +211,209 @@ SignLED ledInfo[NUM_LEDS] = {
 
 void setup() {
   delay(3000); // 3 second delay for recovery
+
+  // set a random random seed
+  random16_set_seed(analogRead(0));
   
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 }
 
-
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-//SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-SimplePatternList gPatterns = { rainbow };
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gCurrentEffect = 0;
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-unsigned long gRestStart = 0;
-bool gRestOn = true;
 
+uint32_t gEffectSpacing = 0;
+uint32_t gLastEffectStart = 0;
 
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+  basePattern();
 
+  runEffect();
+
+  checkForNewEffect();
+  
   // send the 'leds' array out to the actual LED strip
   FastLED.show();  
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND); 
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
-  EVERY_N_SECONDS( 8 ) { cycleRest(); }
-
+  EVERY_N_MILLISECONDS( 50 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-void nextPattern()
+void basePattern() 
 {
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
-}
-
-void rainbow() 
-{
-  // FastLED's built-in rainbow generator
-  //fill_rainbow( leds, NUM_LEDS, gHue, 7);
   for(int i=0; i< NUM_LEDS; i++) {
-    uint8_t hue = gHue + letterNum(i) * 256 / 6;
+    uint8_t hue = gHue + globalLetterNum(i) * 256 / 6;
     leds[i] = CHSV(hue, 255, 255);
   }
-  blankRest();
 }
 
-void cycleRest() {
-  gRestOn = !gRestOn;
-  gRestStart = millis();
-}
+/*********************************
+ * Effects
+ *********************************/
 
-void blankRest() {
-  long total_time = 0;
-  int flicker_step;
-  for(flicker_step = 0; flicker_step<ARRAY_SIZE(flicker_intervals); flicker_step++) {
-    total_time += flicker_intervals[flicker_step];
-    if ( total_time > millis() - gRestStart ) {
-      break;
-    }
-  }
-  if (flicker_step % 2 == (gRestOn ? 1 : 0)) {
+void fuckYou() { 
+  if (flickerStep() % 2 == (currentEffectTime() < (FUCK_YOU_DURATION * 1000) ? 0 : 1)) {
     for(int i=0; i< NUM_LEDS; i++) {
-      if( letterNum(i) > 6 ) {
-        leds[i] = CHSV(0, 255, 0);
+      SignLED myLED = ledInfo[i];
+      if( myLED.word_num > 2 || (myLED.word_num == 1 && myLED.let_num == 3) ) {
+        leds[i] = 0;
       }
     }
   }
 }
 
-uint8_t letterNum(uint8_t index) {
-  if(index < 9) {
-    return 0;
-  } else if(index < 20) {
-    return 1;
-  } else if(index < 29) {
-    return 2;
-  } else if(index < 40) {
-    return 3;
-  } else if(index < 53) {
-    return 7;
-  } else if(index < 64) {
-    return 6;
-  } else if(index < 74) {
-    return 5;
-  } else if(index < 83) {
-    return 4;
-  } else if(index < 99) {
-    return 12;
-  } else if(index < 112) {
-    return 8;
-  } else if(index < 123) {
-    return 9;
-  } else if(index < 136) {
-    return 10;
+void heartYou() {
+  if (flickerStep() % 2 == (currentEffectTime() < (HEART_YOU_DURATION * 1000) ? 0 : 1)) {
+    for(int i=0; i< NUM_LEDS; i++) {
+      SignLED myLED = ledInfo[i];
+      if( myLED.word_num % 2 == 0  || myLED.let_num == 3 ) {
+        leds[i] = 0;
+      }
+    }
+  }
+}
+
+void fuckOurBurn() {
+  if (flickerStep() % 2 == (currentEffectTime() < (FUCK_OUR_BURN_DURATION * 1000) ? 0 : 1)) {
+    for(int i=0; i< NUM_LEDS; i++) {
+      SignLED myLED = ledInfo[i];
+      if(( myLED.word_num == 1  && myLED.let_num == 3 ) || myLED.word_num == 3 ) {
+        leds[i] = 0;
+      }
+    }
+  }
+}
+
+void sparkleWave() {
+  // line of form a*x + b*y - c = 0
+  const uint8_t a = 40;
+  const uint8_t b = 50;
+
+  uint16_t c = currentEffectTime();
+  
+  for(int i=0; i< NUM_LEDS; i++) {
+    SignLED myLED = ledInfo[i];
+    uint16_t distance = abs(a * myLED.x + b * myLED.y - c)/64;
+    uint8_t sparkleChance = MAX_SPARKLE_CHANCE * (MAX_SPARKLE_DISTANCE - distance) / MAX_SPARKLE_DISTANCE;
+    if (sparkleChance < random8(100)) {
+      leds[i] = CRGB::White;
+    }
+  }
+}
+
+/**************************************
+ * Effect control and timing
+ ****************************************/
+
+void runEffect() {
+  switch (gCurrentEffect) {
+    case 0:
+      fuckYou();
+      return;
+    case 1:
+      heartYou();
+      return;
+    case 2:
+      fuckOurBurn();
+      return;
+    case 3:
+      sparkleWave();
+      return;
+  }
+}
+
+void checkForNewEffect() {
+  uint32_t curTime = currentEffectTime();
+  if (curTime < gEffectSpacing) {
+    return;
+  }
+  
+  chooseNewPattern();
+  chooseEffectSpacing();
+  gLastEffectStart = millis();
+}
+
+void chooseNewPattern() {
+  uint8_t roll = random8(100);
+
+  if (roll < 60) {
+    gCurrentEffect = 0;
+  } else if (roll < 80) {
+    gCurrentEffect = 3;
+  } else if (roll < 95) {
+    gCurrentEffect = 1;
   } else {
-    return 11;  
+    gCurrentEffect = 2;
   }
 }
 
-void rainbowWithGlitter() 
-{
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
+void chooseEffectSpacing() {
+  gEffectSpacing = random16(MIN_EVENT_SPACING * 10, MAX_EVENT_SPACING * 10) * 100;
 }
 
-void addGlitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
+
+/**************************************
+ * Helpers
+ ****************************************/
+
+uint8_t flickerStep() {
+  uint32_t total_time = 0;
+  int flicker_step;
+
+  uint32_t effectTime = currentEffectTime();  
+  
+  for(flicker_step = 0; flicker_step<ARRAY_SIZE(flicker_intervals); flicker_step++) {
+    total_time += flicker_intervals[flicker_step];
+    if ( total_time > effectTime ) {
+      return flicker_step;
+    }
   }
 }
 
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+uint32_t currentEffectTime() {
+  return millis() - gLastEffectStart;
 }
 
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
+void maybeGlitterLED(uint8_t index, uint8_t chance){
+  
 }
 
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
+uint8_t globalLetterNum(uint8_t index) {
+  SignLED myLED = ledInfo[index];
+  return myLED.let_num + myLED.word_num * 4;
+//  if(index < 9) {
+//    return 0;
+//  } else if(index < 20) {
+//    return 1;
+//  } else if(index < 29) {
+//    return 2;
+//  } else if(index < 40) {
+//    return 3;
+//  } else if(index < 53) {
+//    return 7;
+//  } else if(index < 64) {
+//    return 6;
+//  } else if(index < 74) {
+//    return 5;
+//  } else if(index < 83) {
+//    return 4;
+//  } else if(index < 99) {
+//    return 12;
+//  } else if(index < 112) {
+//    return 8;
+//  } else if(index < 123) {
+//    return 9;
+//  } else if(index < 136) {
+//    return 10;
+//  } else {
+//    return 11;  
+//  }
 }
